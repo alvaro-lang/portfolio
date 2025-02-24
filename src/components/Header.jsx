7b87cdbea155f56d2837f6dba563fd5aa470031d
Alvaro Pastor Sellers <alvaropers96@gmail.com>
2025-02-24 21:29:08 +0000

    Create retrobutton and change animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../styles/Header.css"
 import { useLocation } from 'react-router-dom';
+import RetroButton from './RetroButton';
 
 export default function Header() {
 
@@ -18,9 +19,9 @@ export default function Header() {
           <li className='page'><a className={isActive('/experience')} href='/experience'>EXPERIENCE</a></li>
           <li className='page'><a className={isActive('/courses')} href='/courses'>COURSES</a></li>
           <li>
-            <div className='resume-button'>
-              <a href='/CV_Alvaro_Pastor_Sellers.pdf' download>RESUME</a>
-            </div>
+            <a href='/CV_Alvaro_Pastor_Sellers.pdf' download>
+              <RetroButton text="RESUME"/>
+            </a>
           </li>
         </ul>
       </nav>
---
+++ src/components/RetroButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/RetroButton.css'
+
+export default function RetroButton({text}) {
+  return (
+    <div id='retro-button'>
+        {text}
+    </div> 
+  )
+}
+
+
