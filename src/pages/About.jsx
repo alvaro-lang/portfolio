b7a1edf4d62f1afac212f0e1ee056ebcbd746324
Alvaro Pastor Sellers <alvaropers96@gmail.com>
2025-02-24 23:01:28 +0000

    finish about design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import '../styles/About.css'
-import TechIcons from '../components/TechIcons'
 
 export default function About() {
 
     return (
         <div id="about">
-            <div className='center-text'>
+            <div className='text-container'>
                 <div className='presentation-title'>
                     Hey, I'm
                 </div>
@@ -20,7 +19,6 @@ export default function About() {
                     Computer engineer specialized in software engineering with more than 2 years of experience in full-stack web application development with Python/Django, JavaScript/Vue.js, HTML5 and CSS3. Extensive experience in the software product creation life cycle, as well as: requirements analysis, system design and architecture, product development, unit testing, deployment to production and product maintenance.              
                 </div>
             </div>
-            <TechIcons/>
         </div>
     )
 }
