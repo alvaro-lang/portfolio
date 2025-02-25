12318115cce1615f0dea6a8e88e39373cebcbd97
Alvaro Pastor Sellers <alvaropers96@gmail.com>
2025-02-25 01:33:22 +0000

    margins fixed and arrow added

---
+++ src/components/DownArrow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/DownArrow.css'
+
+export default function DownArrow() {
+    return (
+        <div id='down-arrow'>
+            <img src='/images/down_arrow.png' alt="Down arrow"></img>
+        </div> 
+    )
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/About.css'
+import DownArrow from '../components/DownArrow'
 
 export default function About() {
 
@@ -19,6 +20,7 @@ export default function About() {
                     Computer engineer specialized in software engineering with more than 2 years of experience in full-stack web application development with Python/Django, JavaScript/Vue.js, HTML5 and CSS3. Extensive experience in the software product creation life cycle, as well as: requirements analysis, system design and architecture, product development, unit testing, deployment to production and product maintenance.              
                 </div>
             </div>
+            <DownArrow/>
         </div>
     )
 }
