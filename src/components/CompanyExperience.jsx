69ac463c6f4eae3cd22f6d0d8675b7d1e3b7c174
Alvaro Pastor Sellers <alvaropers96@gmail.com>
2025-03-04 16:15:30 +0000

    Experience section finished

--- src/components/CompanyExperience.jsx
+++ src/components/CompanyExperience.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../styles/CompanyExperience.css'
 
-export default function CompanyExperience({job_title, date, company, technologies}) {
+export default function CompanyExperience({job_title, date, company, text_info, technologies}) {
   return (
     <div id='company-experience'>
         <div className='date'>
@@ -15,9 +15,7 @@ export default function CompanyExperience({job_title, date, company, technologie
                 {company}
             </div>
             <div className='text-info'>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
+                <p dangerouslySetInnerHTML={{ __html: text_info }}></p>
             </div>
             <div className='container-grid white-font'>
                 {technologies.map((tech, index) => (
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -5,18 +5,21 @@ import CompanyExperience from '../components/CompanyExperience'
 
 export default function Experience() {
 
-  const jakincode_technologies=['Python', 'Django', 'Vue']
-  const revoluz_technologies=['Python', 'Django', 'Vue']
+  const jakincode_text_info = "I developed web applications using technologies such as Django for the backend and Vue.js and React.js for the frontend. These applications were designed with a REST API architecture, utilizing Django Rest Framework to build and manage data interactions between the client and the server effectively. Additionally, I led meetings with clients to define maintenance strategies, ensuring that the applications remained up-to-date and continuously improved with new features. I also mentored junior developers, guiding them in best practices and fostering the development of technical skills and the implementation of high-quality coding standards."
+  const jakincode_technologies=['Python', 'Django', 'Django Rest Framework', 'JavaScript', 'Vue.js', 'React.js', 'Celery', 'Redis', 'Docker']
+  
+  const revoluz_text_info="I developed several web applications using Django for the backend and Vue.js and React.js for the frontend. One of my projects includes an automatic electric bill comparison tool, and I also created interactive maps using Leaflet.js. Additionally, I deployed all of my projects in the cloud using a DigitalOcean VPS."
+  const revoluz_technologies=['Python', 'Django', 'Django Rest Framework', 'JavaScript', 'Vue.js', 'React.js', 'Leaflet.js']
 
   return (
     <section id="experience">
       <SectionTitle text={'EXPERIENCE'}/>
       
       <div className='container-jakincode'>
-        <CompanyExperience job_title='Full-Stack Software Engineer' date='2023 - 2024' company='JakinCode' technologies={jakincode_technologies}/>
+        <CompanyExperience job_title='Full-Stack Software Engineer' date='2023 - 2024' company='JakinCode' text_info={jakincode_text_info} technologies={jakincode_technologies}/>
       </div>
       <div className='container-revoluz'>
-        <CompanyExperience job_title='Full-Stack Software Engineer' date='2022 - 2024' company='Revoluz Energy Consulting' technologies={revoluz_technologies}/>
+        <CompanyExperience job_title='Full-Stack Software Engineer' date='2022 - 2024' company='Revoluz Energy Consulting' text_info={revoluz_text_info} technologies={revoluz_technologies}/>
       </div>
     </section>
   )
