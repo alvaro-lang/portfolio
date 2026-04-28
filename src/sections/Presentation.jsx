411361b95a9cfdf0dbd271a0526b5fba616e4066
Alvaro <alvaropers96@gmail.com>
2026-04-29 00:26:37 +0100

    update presentation

--- src/sections/Presentation.jsx
+++ src/sections/Presentation.jsx
@@ -12,7 +12,7 @@ export default function Presentation() {
                     <span className='container-margin white-font'>Developer</span>
                 </div>
                 <div className="presentation-text description-text">
-                    Hi! I'm <span className='retro-color'>Alvaro</span>. A <span className='retro-color'>Full-Stack Software Developer</span> with 2+ years of experience in web application development with Python (<span className='retro-color'>Django</span>) and JavaScript (<span className='retro-color'>Vue.js, React.js</span>).
+                    Hi! I'm <span className='retro-color'>Alvaro</span>. A <span className='retro-color'>Full-Stack Software Developer</span> with 4+ years of experience in web application development with Python (<span className='retro-color'>Django</span>) and JavaScript (<span className='retro-color'>Vue.js, React.js</span>).
                 </div>
                 <ContactBar/>
             </div>
